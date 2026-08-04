11bc21339ed4baacec01163a6c0fb4471fab77cf
Oleg Korolev <oleg95korolev@gmail.com>
2026-08-04 13:21:02 +0300

    Added FPS initializer

---
+++ Assets/Code/Scene/Installers/FPSInitializer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Code.Scene.Installers
+{
+    public class FPSInitializer : MonoBehaviour
+    {
+        [SerializeField] private int _targetFrameRate = 60;
+        
+        private void Awake()
+        {
+            Application.targetFrameRate = _targetFrameRate;
+        }
+    }
+}
